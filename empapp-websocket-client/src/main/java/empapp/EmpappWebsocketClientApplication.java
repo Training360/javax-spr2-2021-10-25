ba07d237aea9d1952cedd5a9ca65fe6c2f82642e
Istvan Viczian <viczian.istvan@gmail.com>
2021-10-26 15:07:37 +0200

    WebSocket Java kliens done

--- empapp-websocket-client/src/main/java/empapp/EmpappWebsocketClientApplication.java
+++ empapp-websocket-client/src/main/java/empapp/EmpappWebsocketClientApplication.java
@@ -1,13 +1,49 @@
 package empapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class EmpappWebsocketClientApplication {
+public class EmpappWebsocketClientApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpappWebsocketClientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+		//WebSocketClient client = new StandardWebSocketClient();
+		WebSocketClient client = new SockJsClient(
+				Arrays.asList(new WebSocketTransport(new StandardWebSocketClient())));
+		WebSocketStompClient stompClient = new WebSocketStompClient(client);
+		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+		StompSessionHandler sessionHandler = new MessageStompSessionHandler();
+		StompSession stompSession = stompClient.connect("ws://localhost:8080/websocket-endpoint", sessionHandler).get();
+		inputMessages(stompSession);
+	}
+
+	public void inputMessages(StompSession stompSession) {
+		Scanner scanner = new Scanner(System.in);
+		String input = null;
+		while (input == null || !input.trim().equals("")) {
+			input = scanner.nextLine();
+			stompSession.send("/app/messages", new MessageCommandDto(input));
+		}
+	}
 }
---
+++ empapp-websocket-client/src/main/java/empapp/MessageCommandDto.java
@@ -0,0 +1,15 @@
+package empapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageCommandDto {
+
+    private String content;
+
+
+}
